224b9bf4670f8d96199ece58c83da437608ba982
generalentropy <entropie@tuta.io>
2024-01-05 13:01:56 +0100

    add context

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { CitiesProvider } from "./contexts/CitiesContext";
 
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
@@ -9,58 +9,31 @@ import Product from "./pages/Product";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import Form from "./components/Form";
-
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 
-const BASE_URL = "http://localhost:8000";
-
 function App() {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(function () {
-    async function fetchCities() {
-      try {
-        setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
-        const data = await res.json();
-
-        setCities(data);
-      } catch (err) {
-        alert(`There was an error loading data: ${err}`);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-
-    fetchCities();
-  }, []);
-
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Homepage />}></Route>
-        <Route path="product" element={<Product />}></Route>
-        <Route path="pricing" element={<Pricing />}></Route>
-        <Route path="login" element={<Login />}></Route>
-        <Route path="app" element={<AppLayout />}>
-          <Route index element={<Navigate replace to="cities" />}></Route>
-          <Route
-            path="cities"
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          ></Route>
+    <CitiesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Homepage />}></Route>
+          <Route path="product" element={<Product />}></Route>
+          <Route path="pricing" element={<Pricing />}></Route>
+          <Route path="login" element={<Login />}></Route>
 
-          <Route path="cities/:id" element={<City />}></Route>
-          <Route
-            path="countries"
-            element={<CountryList cities={cities} isLoading={isLoading} />}
-          ></Route>
-          <Route path="form" element={<Form />}></Route>
-        </Route>
-        <Route path="*" element={<PageNotFound />}></Route>
-      </Routes>
-    </BrowserRouter>
+          <Route path="app" element={<AppLayout />}>
+            <Route index element={<Navigate replace to="cities" />}></Route>
+            <Route path="cities" element={<CityList />}></Route>
+            <Route path="cities/:id" element={<City />}></Route>
+            <Route path="countries" element={<CountryList />}></Route>
+            <Route path="form" element={<Form />}></Route>
+          </Route>
+
+          <Route path="*" element={<PageNotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </CitiesProvider>
   );
 }
 
--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,10 +1,11 @@
 import Spinner from "./Spinner";
 import styles from "./CountryList.module.css";
-
 import Message from "./Message";
 import CountryItem from "./CountryItem";
+import { useCities } from "../contexts/CitiesContext";
 
-function CountryList({ cities, isLoading }) {
+function CountryList() {
+  const { cities, isLoading } = useCities();
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
