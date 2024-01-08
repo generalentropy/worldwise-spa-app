1632e0d7ffc0ba16ce923f5ffe966cc97e8d3a46
generalentropy <entropie@tuta.io>
2024-01-08 18:24:49 +0100

    upload data

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from "react";
 import { useUrlPosition } from "../hooks/useUrlPosition";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import Button from "./Button";
 import styles from "./Form.module.css";
 import BackButton from "./BackButton";
-import "/node_modules/flag-icons/css/flag-icons.min.css";
+import Message from "./Message";
+import Spinner from "./Spinner";
+import { useCities } from "../contexts/CitiesContext";
+import { useNavigate } from "react-router-dom";
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
@@ -17,28 +22,40 @@ const BASE_URL = `https://api.bigdatacloud.net/data/reverse-geocode-client?`;
 
 function Form() {
   const [isLoadingGeocoding, setIsLoadingGeocoding] = useState(false);
+  const navigate = useNavigate();
+  const { createCity, isLoading } = useCities();
   const [mapLat, mapLng] = useUrlPosition();
   const [cityName, setCityName] = useState("");
   const [country, setCountry] = useState("");
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState("");
+  const [geocodingError, setGeocodingError] = useState("");
 
   const countryISO = country ? country.countryCode.toLowerCase() : "";
 
   useEffect(
     function () {
+      if (!mapLat && !mapLng) return;
+
       async function getCountry() {
         try {
           setIsLoadingGeocoding(true);
+          setGeocodingError("");
           const res = await fetch(
             `${BASE_URL}latitude=${mapLat}&longitude=${mapLng}`
           );
           const data = await res.json();
 
+          if (!data.countryCode)
+            throw new Error(
+              `That doesn't seem to be a city. Click somewhere else.`
+            );
+
           setCityName(data.city);
           setCountry(data);
         } catch (err) {
           console.log(err);
+          setGeocodingError(err.message);
         } finally {
           setIsLoadingGeocoding(false);
         }
@@ -49,26 +66,53 @@ function Form() {
     [mapLat, mapLng]
   );
 
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!cityName || !date) return;
+
+    const newCity = {
+      cityName,
+      country: country.countryName,
+      iso: countryISO,
+      date,
+      notes,
+      position: { lat: mapLat, lng: mapLng },
+    };
+
+    await createCity(newCity);
+    navigate("/app/cities");
+  }
+
+  if (isLoadingGeocoding) return <Spinner />;
+  if (!mapLat && !mapLng)
+    return <Message message="Start by clicking somewhere on the map" />;
+  if (geocodingError) return <Message message={geocodingError} />;
+
   return (
-    <form className={styles.form}>
+    <form
+      className={`${styles.form} ${isLoading ? styles.loading : ""}`}
+      onSubmit={handleSubmit}
+    >
       <div className={styles.row}>
         <label htmlFor="cityName">City name</label>
         <input
           id="cityName"
           onChange={(e) => setCountry(e.target.value)}
-          value={isLoadingGeocoding ? "Loading..." : cityName}
+          value={cityName}
         />
 
         <span className={`fi fi-${countryISO} ${styles.flag}`}></span>
-        {/* <span className={styles.flag}>{emoji}</span> */}
       </div>
 
       <div className={styles.row}>
         <label htmlFor="date">When did you go to {cityName}?</label>
-        <input
+
+        <DatePicker
           id="date"
-          onChange={(e) => setDate(e.target.value)}
-          value={date}
+          onChange={(date) => setDate(date)}
+          selected={date}
+          dateFormat="dd/MM/yyyy"
         />
       </div>
 
--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -41,6 +41,24 @@ function CitiesProvider({ children }) {
     }
   }
 
+  async function createCity(newCity) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities`, {
+        method: "POST",
+        body: JSON.stringify(newCity),
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await res.json();
+      setCities((cities) => [...cities, data]);
+      console.log(data);
+    } catch (err) {
+      alert(`There was an error sending data: ${err}`);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <CitiesContext.Provider
       value={{
@@ -48,6 +66,7 @@ function CitiesProvider({ children }) {
         isLoading,
         currentCity,
         getCity,
+        createCity,
       }}
     >
       {children}
